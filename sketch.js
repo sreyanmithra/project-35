59b34cbd5d2c35b53ba1ba5ee4e3474644513724
sreyanmithra <71690686+sreyanmithra@users.noreply.github.com>
2021-05-08 13:41:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,92 @@
+var balloon,balloonImage1,balloonImage2;
+
+var database
+var position
+
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+ 
+  createCanvas(1500,700)
+
+  database=firebase.database()
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20)
+
+  var positionofchild=database.ref("balloon/positions")
+
+    positionofchild.on("value",readop,showerror)
+
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    writePosition(-1,0);
+
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    writePosition(1,0);
+
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    writePosition(0,-1);
+
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    writePosition(0,+1);
+
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function writePosition(x,y){
+
+  database.ref("balloon/positions").set({
+      x:balloon.x+x,
+      y:balloon.y+y
+  })
+  
+}
+
+function readop(data){
+
+  position=data.val()
+
+  balloon.x=position.x
+  balloon.y=position.y
+
+}
+
+function showerror(){
+
+  console.log("error")
+
+}
